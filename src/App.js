66226a139fa2c172da1bd97a47654a9e7e19bd79
Daniel Barwén Trollvik <Daniel.Barwen.Trollvik-CIC@ibm.com>
2023-03-26 16:52:22 +0200

    yep

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from "react-dom/client"; //New
-import { BrowserRouter, Routes, Route } from "react-router-dom"; //New
-import './App.scss';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import ErrorPage from './pages/ErrorPage';
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
+import ErrorPage from './pages/ErrorPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="error" element={<ErrorPage />} />
-          <Route path="*" element={<ErrorPage />} />
+          {/* <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
@@ -23,41 +24,3 @@ export default function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-
-
-
-
-
-
-
-
-// const routes = [
-//   {
-//     path: '/',
-//     element: <Home />,
-//     errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: 'home',
-//     element: <Home />,
-//     errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: 'error',
-//     element: <ErrorPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-// ];
-
-// const router = createHashRouter(routes);
-
-// const App = () => {
-//   return (
-//     <div className='App'>
-//       <RouterProvider router={router} />
-//     </div>
-//   );
-// };
-
-// export default App;
